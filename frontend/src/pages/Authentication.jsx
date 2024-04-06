3b2fb11b7052d6312617b4504289dec226fa5e14
Dhavisco <olaniyidavid2002@gmail.com>
2024-04-06 18:41:30 +0100

    create mode in action

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -7,6 +7,9 @@ function AuthenticationPage() {
 export default AuthenticationPage;
 
 export async function action({ request }) {
+
+ const searchParams = new URL(request.url).searchParams;
+ const mode = searchParams.get('mode') || 'login';
   const data = await request.formData();
   const authData = {
     email: data.get("email"),
