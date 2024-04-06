664b236a2f66a42a9e9fb9e2f41c131a1d745c13
Dhavisco <olaniyidavid2002@gmail.com>
2024-04-06 18:56:16 +0100

    redirect if successful

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -1,4 +1,4 @@
-import { json } from "react-router-dom";
+import { json, redirect } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
@@ -8,14 +8,12 @@ function AuthenticationPage() {
 export default AuthenticationPage;
 
 export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
 
- const searchParams = new URL(request.url).searchParams;
- const mode = searchParams.get('mode') || 'login';
-
-
- if(mode !== 'login' && mode !== 'signup'){
-  throw json({message: 'Unsuppported mode.'}, {status: 422});
- }
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsuppported mode." }, { status: 422 });
+  }
 
   const data = await request.formData();
   const authData = {
@@ -23,20 +21,20 @@ export async function action({ request }) {
     password: data.get("password"),
   };
 
- const response = await fetch('http://localhost:8080/' + mode, {
-  method:'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(authData)
-});
- if (response.status === 422 || response.status === 401){
-  return response; 
- }
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
 
- if(!response.ok){
-  throw json({message:'Could not authenticate user.'}, {status:500});
- }
-
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user." }, { status: 500 });
+  }
+  // soon: manage that token
+  return redirect("/");
 }
-
